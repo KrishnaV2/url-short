23060e267acf9b2b452186dbd0ec998413090708
KrishnaV2 <krishnavijaywargia007@gmail.com>
2024-10-05 21:04:45 +0530

    Backend Setup

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const URL = require("./db");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors"
+import dotenv from "dotenv"
+import URL from "./db.js"
 
 const app = express();
 
@@ -16,14 +16,33 @@ app.options('*', cors())
 dotenv.config({ path: "./.env.local" })
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("Connected to DB"))
 
-app.post('/url', function (req, res) {
-    const { url } = req.body;
-
-
-})
-app.get('/:urlId', function (req, res) {
-
-})
-
+app.post('/api/v1/url', postURL)
+app.get('/api/v1/url/:urlId', getURL)
+async function postURL(req, res) {
+    try {
+        const { url } = req.body;
+        if (!url) return res.status(400).json({ "status": "fail", "message": "URL is missing" });
+        let data = await URL.findOne({ url });
+        if (data) return res.status(200).json({ "status": "success", "id": data.urlId });
+        data = await URL.create({ url });
+        if (!data) return res.status(200).json({ "status": "fail", "message": "URL could not be created. Try Again." });
+        return res.status(200).json({ "status": "success", "id": data.urlId });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ "status": "fail", "message": "Internal Server Error" });
+    }
+}
+async function getURL(req, res) {
+    try {
+        const { urlId } = req.params
+        if (!urlId) return res.status(400).json({ "status": "fail", "message": "URL ID is missing" });
+        const data = await URL.findOne({ urlId });
+        if (!data) return res.status(200).json({ "status": "fail", "message": "No URL found with given ID" });
+        return res.status(200).json({ "status": "success", "url": data.url });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ "status": "fail", "message": "Internal Server Error" });
+    }
+}
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log("Server Started at " + PORT))
--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const nanoid = require("nanoid");
+import mongoose from "mongoose"
+import { nanoid } from "nanoid";
 
 const URLSchema = new mongoose.Schema({
     url: {
@@ -7,11 +7,11 @@ const URLSchema = new mongoose.Schema({
         required: [true, "A URL is required to shorten it."]
     },
     urlId: {
-        type: UUID,
+        type: String,
         default: nanoid(7),
+        unique: true
     }
 })
 
 const URL = mongoose.model('url', URLSchema);
-
-export default URL;
+export default URL
